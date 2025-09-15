0f2adc10774ec244b51786f102c91bb73da7ab3f
Victoria Ainbinder <victoriammnm49@gmail.com>
2025-09-15 19:20:58 +0300

    change headless

--- conftest.py
+++ conftest.py
@@ -6,6 +6,7 @@ from selenium.webdriver.chrome.options import Options
 def browser():
     options = Options()
     options.add_argument("--start-maximized")
+    options.add_argument("--headless=new")
     # при желании: options.add_argument("--disable-dev-shm-usage"); options.add_argument("--no-sandbox")
     driver = webdriver.Chrome(options=options)  # без Service/manager
     yield driver
